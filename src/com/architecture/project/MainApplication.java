7c2203359635e4aa703141e4e644db92ee4027a9
Amanda RÉTORY <retorya.amanda@gmail.com>
2022-07-08 17:52:48 -0400

    add running configuration

---
+++ src/com/architecture/project/MainApplication.java
@@ -0,0 +1,8 @@
+package com.architecture.project;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome in a functioning project !!! :)");
+    }
+}
