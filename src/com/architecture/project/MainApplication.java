3b29f74d33e4e49d969893891ee05544b5243a34
Marmorand31 <47538734+Marmorand31@users.noreply.github.com>
2022-07-09 14:51:53 +0200

    Gradle

--- src/com/architecture/project/MainApplication.java
+++ src/com/architecture/project/MainApplication.java
@@ -1,14 +1,48 @@
 package com.architecture.project;
 
-public class MainApplication {
-    public static void main(String[] args) {
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
 
-        System.out.println("Welcome in a functioning project !!! :)");
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainApplication {
+    public static void main(String[] args) throws IOException {
+
+        /*System.out.println("Welcome in a functioning project !!! :)");
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault())
                 .setDatabaseUrl("https://gestionclientele-72f65-default-rtdb.europe-west1.firebasedatabase.app/")
                 .build();
 
+        FirebaseApp.initializeApp(options);*/
+
+        // Fetch the service account key JSON file contents
+        FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccount.json");
+
+// Initialize the app with a service account, granting admin privileges
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                // The database URL depends on the location of the database
+                .setDatabaseUrl("https://DATABASE_NAME.firebaseio.com")
+                .build();
         FirebaseApp.initializeApp(options);
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+        DatabaseReference ref = FirebaseDatabase.getInstance()
+                .getReference("restricted_access/secret_document");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Object document = dataSnapshot.getValue();
+                System.out.println(document);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
     }
 }
