23014bb599caab8da2fd51ea313d155ffbccb46f
Marmorand31 <47538734+Marmorand31@users.noreply.github.com>
2022-07-09 12:13:51 +0200

    Update MainApplication.java

--- src/com/architecture/project/MainApplication.java
+++ src/com/architecture/project/MainApplication.java
@@ -4,5 +4,11 @@ public class MainApplication {
     public static void main(String[] args) {
 
         System.out.println("Welcome in a functioning project !!! :)");
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setDatabaseUrl("https://gestionclientele-72f65-default-rtdb.europe-west1.firebasedatabase.app/")
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 }
