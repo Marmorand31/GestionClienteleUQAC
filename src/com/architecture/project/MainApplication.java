dd17bd60c48c263b50c753351ff0eca785e13f6d
Marmorand31 <47538734+Marmorand31@users.noreply.github.com>
2022-07-09 16:17:02 +0200

    Revert "Update MainApplication.java"
    This reverts commit 23014bb599caab8da2fd51ea313d155ffbccb46f.

--- src/com/architecture/project/MainApplication.java
+++ src/com/architecture/project/MainApplication.java
@@ -4,11 +4,5 @@ public class MainApplication {
     public static void main(String[] args) {
 
         System.out.println("Welcome in a functioning project !!! :)");
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
-                .setDatabaseUrl("https://gestionclientele-72f65-default-rtdb.europe-west1.firebasedatabase.app/")
-                .build();
-
-        FirebaseApp.initializeApp(options);
     }
 }
